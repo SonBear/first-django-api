a4724d23ee0155c7ada7320472a5fee27c9172c0
SonBear <emmanuelisai_28@hotmail.com>
2023-02-19 17:45:59 -0600

    format json response

--- polls/models.py
+++ polls/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=200)
     school_id = models.CharField(max_length=200)
+    def format(self):
+        return {'id': self.pk, 'name': self.name, 'school_id': self.school_id}
--- polls/views.py
+++ polls/views.py
@@ -6,15 +6,15 @@ from django.http.response import JsonResponse
 
 def index_students(request):
     students = Student.objects.all()
-    serialized_students = serializers.serialize('json', students)
-    return JsonResponse(serialized_students, status=200, safe=False)
+    students = map(lambda x: x.format(), list(students))
+    students = list(students)
+    return JsonResponse(list(students), status=200, safe=False)
 
 def save_students(request):
     student = Student(name='Emmanuel Chable', school_id='18016328')
     student.save()
     serialized_student = serializers.serialize('json', [student])
     return JsonResponse(serialized_student, status=200, safe=False)
-
 
 
 
